542f1883a3617e95fb5fbb995ffa7d7dfd2413c1
Rob Burger <rob.burgs@gmail.com>
2021-01-25 22:22:47 +0200

    sed version problem?

--- entrypoint.sh
+++ entrypoint.sh
@@ -183,7 +183,7 @@ if [[ $COMMAND == 'plan' ]]; then
   # Meaning: 0 = Terraform plan succeeded with no changes. 2 = Terraform plan succeeded with changes.
   # Actions: Strip out the refresh section, ignore everything after the 72 dashes, format, colourise and build PR comment.
   if [[ $EXIT_CODE -eq 0 || $EXIT_CODE -eq 2 ]]; then
-    CLEAN_PLAN=$(echo "$INPUT" | sed -n '/Refreshing state\.\.\./!p') # Strip refresh section
+    CLEAN_PLAN=$(echo "$INPUT" | sed -E '0,/Refreshing state\.\.\./d') # Strip refresh section
     CLEAN_PLAN=$(echo "$INPUT" | sed -nE '/-{72}/q;p') # Ignore everything after the 72 dashes (happens when saving a plan to file)
     CLEAN_PLAN=${CLEAN_PLAN::65300} # GitHub has a 65535-char comment limit - truncate plan, leaving space for comment wrapper
     CLEAN_PLAN=$(echo "$CLEAN_PLAN" | sed -E 's/^([[:blank:]]*)([-+~])/\2\1/g') # Move any diff characters to start of line
