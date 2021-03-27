addb4c5d1c6d3c8e7496f53dd39279e4b72a252d
Rob Burger <rob.burgs@gmail.com>
2021-03-27 13:54:44 +0200

    Revert comment change

--- entrypoint.sh
+++ entrypoint.sh
@@ -181,7 +181,7 @@ if [[ $COMMAND == 'plan' ]]; then
 
   # Exit Code: 0, 2
   # Meaning: 0 = Terraform plan succeeded with no changes. 2 = Terraform plan succeeded with changes.
-  # Actions: Strip out the refresh section and everything after the 72 dashes, format, colourise and build PR comment.
+  # Actions: Strip out the refresh section, ignore everything after the 72 dashes, format, colourise and build PR comment.
   if [[ $EXIT_CODE -eq 0 || $EXIT_CODE -eq 2 ]]; then
     CLEAN_PLAN=$(echo "$INPUT" | sed -r '/^(An execution plan has been generated and is shown below.|No changes. Infrastructure is up-to-date.)$/,$!d') # Strip refresh section
     CLEAN_PLAN=$(echo "$CLEAN_PLAN" | sed -nr '/-{72}/q;p') # Ignore everything after the 72 dashes (happens when saving a plan to file)
