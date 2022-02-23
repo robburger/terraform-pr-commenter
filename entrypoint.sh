198fbf5bcddf6baec3b6a5793c98c74cf5853ef7
Jonathan Ogilvie <jonathan.ogilvie@terminus.com>
2022-02-23 14:32:33 -0500

    Revise composite action

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@
 #############
 # Validations
 #############
-GITHUB_EVENT_PATH=event.json
-PR_NUMBER=$(jq -r ".pull_request.number" "$GITHUB_EVENT_PATH")
+PR_NUMBER=$(echo "$GITHUB_EVENT" | jq -r ".pull_request.number")
 if [[ "$PR_NUMBER" == "null" ]]; then
 	echo "This isn't a PR."
 	exit 0
@@ -56,8 +55,8 @@ ACCEPT_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 CONTENT_HEADER="Content-Type: application/json"
 
-PR_COMMENTS_URL=$(jq -r ".pull_request.comments_url" "$GITHUB_EVENT_PATH")
-PR_COMMENT_URI=$(jq -r ".repository.issue_comment_url" "$GITHUB_EVENT_PATH" | sed "s|{/number}||g")
+PR_COMMENTS_URL=$(echo "$GITHUB_EVENT" | jq -r ".pull_request.comments_url")
+PR_COMMENT_URI=$(echo "$GITHUB_EVENT" | jq -r ".repository.issue_comment_url" | sed "s|{/number}||g")
 
 ##############
 # Handler: fmt
